b7fd4d20ee47cc8c3f3bea18d06656abad91a59d
kshramt <thisisdummy@example.com>
2013-02-24 02:22:35 +0900

    Add tests, documents and pre-conditions

--- src/sicp/core.clj
+++ src/sicp/core.clj
@@ -1,6 +1,9 @@
-(ns sicp.core)
+(ns sicp.core
+  (:require [clojure.test :as c.t])
+  (:require [clojure.pprint]))
 
 (defn p
+  "Pretty print and return a value"
   [x]
   (clojure.pprint/pprint x)
   x)
@@ -31,11 +34,19 @@
         guess
         (recur x (improved-guess guess))))))
 
-(defn fast-expt2-iter [b n a]
-    (cond
-     (zero? n) a
-     (even? n) (fast-expt2-iter (square b) (half n) a)
-     :else (fast-expt2-iter b (dec n) (* a b))))
+(defn fast-expt2-iter
+  "Substance of fast-expt2."
+  {:test #(do (c.t/is (= (fast-expt2-iter 3 4 1) 81)))}
+  [b n a]
+  {:pre [(>= n 0)]}
+  (cond
+   (zero? n) a
+   (even? n) (fast-expt2-iter (square b) (half n) a)
+   :else (fast-expt2-iter b (dec n) (* a b))))
 
-(defn fast-expt2 [b n]
+(defn fast-expt2
+  "Yet another fast exponential"
+  {:test #(do (c.t/is (= (fast-expt2 3 4) 81)))}
+  [b n]
+  {:pre [(>= n 0)]}
   (fast-expt2-iter b n 1))
