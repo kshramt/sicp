104f69d0689588b90417919b91182ee9a98a2fae
kshramt <thisisdummy@example.com>
2013-02-27 13:57:09 +0900

    Q 1.19

--- src/sicp/core.clj
+++ src/sicp/core.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer [is are run-tests]])
   (:require [clojure.pprint]))
 
-(defn p
+(defn p_
   "Pretty print and return a value"
   [x]
   (clojure.pprint/pprint x)
@@ -94,5 +94,42 @@
                    1 0 1
                    3 4 81)
               (is (thrown? java.lang.AssertionError (my-expt-with-my-* 3 -1))))})
+
+(defn fib
+  "Q 1.19"
+  {:test #(do (are [n result] (= (fib n) result)
+                   1 1
+                   2 1
+                   3 2
+                   4 3
+                   5 5
+                   6 8
+                   7 13
+                   8 21
+                   9 34
+                   10 55)
+              (is (thrown? java.lang.AssertionError (fib 0))))}
+
+  [n]
+  {:pre [(>= n 1)]}
+
+  (letfn [(fib-iter
+            [a b p q n]
+            {:pre [(>= n 0)]}
+
+            (loop [a a b b p p q q n n]
+              (cond
+               (= n 0) b
+               (odd? n) (fib-iter (+ (* b q) (* a (+ p q)))
+                                  (+ (* b p) (* a q))
+                                  p
+                                  q
+                                  (dec n))
+               :else (recur a
+                            b
+                            (+ (* p p) (* q q))
+                            (+ (* 2 p q) (* q q))
+                            (/ n 2)))))]
+    (fib-iter 1 0 0 1 n)))
 
 (run-tests)
