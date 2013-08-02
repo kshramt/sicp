c619e2d02f82ddc96a1b340087882012da9c61fe
kshramt <thisisdummy@example.com>
2013-08-02 19:54:10 +0900

    Update clojure

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject sicp/sicp "0.1.0-SNAPSHOT" 
   :license {:name "GNU General Public License Version 3",
             :url "http://www.gnu.org/licenses/gpl.html"}
-  :dependencies [[org.clojure/clojure "1.5.0-alpha5"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.trace "0.7.3"]]
   :min-lein-version "2.0.0"
   :description "SICP in Clojure")
