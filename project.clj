4553469874994857764dd0345d2f363dcecd9fda
kshramt <thisisdummy@example.com>
2013-08-31 00:56:34 +0900

    Add org.clojure/core.typed to the dependencies

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
             :url "http://www.gnu.org/licenses/gpl.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.trace "0.7.3"]
-                 [org.clojure/math.numeric-tower "0.0.2"]]
+                 [org.clojure/math.numeric-tower "0.0.2"]
+                 [org.clojure/core.typed "0.2.1"]]
   :min-lein-version "2.0.0"
   :description "SICP in Clojure")
