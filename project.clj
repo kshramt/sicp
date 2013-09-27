057e144c80910aeacd146f74751e12255ed29954
kshramt <thisisdummy@example.com>
2013-09-27 22:27:29 +0900

    Update `core.typed`

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.trace "0.7.3"]
                  [org.clojure/math.numeric-tower "0.0.2"]
-                 [org.clojure/core.typed "0.2.10"]]
+                 [org.clojure/core.typed "0.2.13"]]
   :min-lein-version "2.0.0"
   :description "SICP in Clojure"
   :global-vars {*warn-on-reflection* true}
