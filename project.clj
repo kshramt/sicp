5db306a9382355a06ae2163abd71dcdb1401b1b8
kshramt <thisisdummy@example.com>
2013-10-17 23:02:43 +0900

    Update a dependency

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :license {:name "GNU General Public License Version 3",
             :url "http://www.gnu.org/licenses/gpl.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.trace "0.7.3"]
+                 [org.clojure/tools.trace "0.7.6"]
                  [org.clojure/math.numeric-tower "0.0.2"]
                  [org.clojure/core.typed "0.2.13"]]
   :min-lein-version "2.0.0"
