c4c3e2f43f7977d60ec68e16a2dc56e182cae671
kshramt <thisisdummy@example.com>
2013-11-02 17:05:53 +0900

    Rename `square-limit'` -> `square-limit`

--- src/sicp/core.clj
+++ src/sicp/core.clj
@@ -2246,8 +2246,8 @@ n^n
                                  identity flip-vert)]
     (combine4 painter)))
 
-(ann square-limit' [Painter Int -> Painter])
-(defn square-limit' [painter n]
+(ann square-limit [Painter Int -> Painter])
+(defn square-limit [painter n]
   (let [combine4 (square-of-four flip-horiz identity
                                  rotate180 flip-vert)]
     (combine4 (corner-split painter n))))
