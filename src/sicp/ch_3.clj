dbbae8fbfaecb6dff0942c9c2d8fa711bf8a9e71
kshramt <thisisdummy@example.com>
2014-09-21 00:24:13 +0900

    3.2, 3.3 and 3.4

--- src/sicp/ch_3.clj
+++ src/sicp/ch_3.clj
@@ -3,8 +3,12 @@
             [clojure.pprint]
             [clojure.core.typed :refer [ann
                                         fn> ; `typed/fn` is not usable in REPL
+                                        letfn>
                                         Int Num
                                         Keyword
+                                        Value
+                                        IFn
+                                        All
                                         ] :as typed]
             ;:verbose
             )
@@ -13,7 +17,7 @@
 
 (ann make-account [Num -> [Keyword -> [Num -> Num]]])
 (defn make-account [balance]
-  (let [local-balance (atom balance)]
+  (let [local-balance (typed/atom :- Num balance)]
     (typed/fn [m :- Keyword]
       (case m
         :withdraw (typed/fn [amount :- Num]
@@ -23,16 +27,95 @@
                         (throw (Exception. "Insufficient funds")))))
         :deposit (typed/fn [amount :- Num]
                    (reset! local-balance (+ @local-balance amount)))
-        :else (throw (Exception. (str "Unknown request: " m)))))))
+        (throw (Exception. (str "Unknown request: " m)))))))
 
 
 (ann make-accumulator [Num -> [Num -> Num]])
 (defn make-accumulator
   "Q. 3.1"
   [x]
-  (let [sum_ (atom x)]
+  (let [sum_ (typed/atom :- Num x)]
     (typed/fn [x :- Num]
       (reset! sum_ (+ @sum_ x)))))
+
+
+(ann ^:no-check make-monitored
+     (All [a b]
+          [[a -> b] -> (IFn [(Value :how-many-calls?) -> Int]
+                            [(Value :reset-count) -> (Value 0)]
+                            [a -> b])]))
+(defn make-monitored
+  "Q. 3.2"
+  [f]
+  (let [counter (typed/atom :- Int 0)]
+    (letfn> [fm :- (IFn [(Value :how-many-calls?) -> Int]
+                        [(Value :reset-count) -> (Value 0)]
+                        [a -> b])
+             (fm [x]
+                 (case x
+                  :how-many-calls? @counter
+                  :reset-count (reset! counter 0)
+                  (do (reset! counter (inc @counter))
+                      (f x))))]
+      fm)))
+
+
+(ann make-protected-account [Num String -> [Keyword -> [Num String -> Num]]])
+(defn make-protected-account
+  "Q. 3.3"
+  [balance password]
+  (let [local-balance (typed/atom :- Num balance)]
+    (typed/fn [m :- Keyword]
+      (case m
+        :withdraw (typed/fn [amount :- Num
+                             pass :- String]
+                    (if (= pass password)
+                      (let [b @local-balance]
+                        (if (>= b amount)
+                          (reset! local-balance (- b amount))
+                          (throw (Exception. "Insufficient funds"))))
+                      (throw (Exception. "Invalid password"))))
+        :deposit (typed/fn [amount :- Num
+                            pass :- String]
+                   (if (= pass password)
+                     (reset! local-balance (+ @local-balance amount))
+                     (throw (Exception. "Invalid password"))))
+        (throw (Exception. (str "Unknown request: " m)))))))
+
+
+(ann make-protected-account-3-4 [Num String -> [Keyword -> [Num String -> Num]]])
+(defn make-protected-account-3-4
+  "Q. 3.4"
+  [balance password]
+  (let [local-balance (typed/atom :- Num balance)
+        n-invalid-password (typed/atom :- Int 0)]
+    (typed/fn [m :- Keyword]
+      (case m
+        :withdraw (typed/fn [amount :- Num
+                             pass :- String]
+                    (if (= pass password)
+                      (let [b @local-balance]
+                        (reset! n-invalid-password 0)
+                        (if (>= b amount)
+                          (reset! local-balance (- b amount))
+                          (throw (Exception. "Insufficient funds"))))
+                      (let [n @n-invalid-password]
+                        (throw (if (>= n 6)
+                                 (Exception. "Call the police")
+                                 (do (reset! n-invalid-password (inc n))
+                                     (Exception. "Invalid password")))))))
+        :deposit (typed/fn [amount :- Num
+                            pass :- String]
+                   (if (= pass password)
+                     (do (reset! n-invalid-password 0)
+                         (reset! local-balance (+ @local-balance amount)))
+                     (let [n @n-invalid-password]
+                         (throw (if (>= n 6)
+                                  (Exception. "Call the police")
+                                  (do (reset! n-invalid-password (inc n))
+                                      (Exception. "Invalid password")))))))
+        (throw (Exception. (str "Unknown request: " m)))))))
+
 
 
 (typed/check-ns)
