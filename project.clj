9cf5d40e43999df16f1fb8e8312376c70c076e09
kshramt <thisisdummy@example.com>
2015-01-15 00:35:04 +0900

    Update `clojure.math.numeric-tower` and `core.typed`

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
                  [org.clojure/tools.trace "0.7.6"]
-                 [org.clojure/math.numeric-tower "0.0.2"]
-                 [org.clojure/core.typed "0.2.73-SNAPSHOT"]]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [org.clojure/core.typed "0.2.77"]]
   :min-lein-version "2.0.0"
   :description "SICP in Clojure"
   :global-vars {*warn-on-reflection* true}
