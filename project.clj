d8c3f9f7016b3729fc970a3372242613fccae203
kshramt <thisisdummy@example.com>
2015-06-20 23:44:26 +0900

    Update

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject sicp/sicp "0.1.0-SNAPSHOT"
   :license {:name "GNU General Public License Version 3",
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
-                 [org.clojure/tools.trace "0.7.6"]
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                 [org.clojure/tools.trace "0.7.8"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [org.clojure/core.typed "0.2.86"]]
+                 [org.clojure/core.typed "0.3.0-alpha5"]]
   :min-lein-version "2.0.0"
   :description "SICP in Clojure"
   :global-vars {*warn-on-reflection* true}
