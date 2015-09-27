f289b5b2dd18c585f25a1b4afb73258af565bcc2
kshramt <thisisdummy@example.com>
2015-09-28 07:46:45 +0900

    More tests

--- scheme.scm
+++ scheme.scm
@@ -3,6 +3,16 @@
  (= (_eval '(begin (define (f a b) (- a b) (+ a b)) (f 1 2)) (setup-environment)) 3) "\n"
  (= (_eval '(begin (define (f x) (define (g x) x) (g x)) (f 1)) (setup-environment)) 1) "\n"
  (= (_eval '(begin (define (f x) (define (g x) x) (g x)) (f 1)) (setup-environment)) 1) "\n"
+ (= (_eval '(begin (define (factorial n)
+                     (define (impl k ret)
+                       (if (= k 1)
+                           ret
+                           (impl (- k 1) (* k ret))))
+                     (impl n 1))
+                   (factorial 7))
+           (setup-environment))
+    5040)
+ "\n"
  (= (_eval '((lambda (n)
                ((lambda (fib)
                   (fib fib n))
@@ -11,9 +21,10 @@
                       1
                       (+ (fb fb (- n 1))
                          (fb fb (- n 2)))))))
-             6)
+             7)
            (setup-environment))
-    13) "\n"
+    21)
+ "\n"
  )
 ;; (begin (driver-loop (setup-environment) (current-input-port))
 ;;        'bey)
